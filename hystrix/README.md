4f51c3851860ed94d2aed152e2169e1ae7e22c52
Scott Robertson <scott.robertson@saic.com>
2018-12-31 13:49:07 -0600

    added hystrix example

---
+++ hystrix/README.md
@@ -0,0 +1,4 @@
+# Spring Cloud Netflix
+## Hystrix Circuit Breaker
+
+https://www.baeldung.com/spring-cloud-netflix-hystrix
---
+++ hystrix/rest-producer/src/main/java/com/scottrbrtsn/cloud/netflix/hystrix/rest/producer/GreetingControllerImpl.java
@@ -0,0 +1,12 @@
+package com.scottrbrtsn.cloud.netflix.hystrix.rest.producer;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingControllerImpl implements GreetingController {
+    @Override
+    public String greeting(@PathVariable("username") String username) {
+        return String.format("Hello %s!\n", username);
+    }
+}
